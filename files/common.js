fcd8dd7454ea191ed5c713c6f8348ff93a09aed6
hottah <info@hottah.jp>
2021-09-04 20:39:47 +0900

    Update common.js

--- files/common.js
+++ files/common.js
@@ -1,3 +1,20 @@
+$(function(){
+
+	$("#header").on("click",function(){
+		if( $("#keypad").length ){
+			$("#keypad").fadeOut(30).remove();
+		}else{
+			$('<div id="keypad"><img src="/d-match/files/keypad.png" /><i></i></div>')
+			.appendTo("body").hide().fadeIn(30);			
+
+			setInterval(function(){
+				$("#keypad i").fadeOut(300,function(){$(this).fadeIn(300)});
+			},300);
+
+		}
+	});
+});
+
 /*
 ==================================================================
 Interface elements for jQuery
